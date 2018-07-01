e8e12af93ec7436e13210e567d70a7a16d1a8274
Mais Eid <meid@aspire-services.net>
2018-07-01 12:31:28 +0300

    Commit 1

--- configuration/env/development.secret.js
+++ configuration/env/development.secret.js
@@ -10,10 +10,10 @@ module.exports = {
     },
   },
   sequalizer: {
-    host: 'localhost',
-    database: 'aspire-slack',
-    username: 'postgres',
-    password: 'admin',
+    host: 'ec2-54-235-244-185.compute-1.amazonaws.com',
+    database: 'd32n0ji74ul5nq',
+    username: 'frvlmckhgbjkzf',
+    password: 'b9351aa92cfd386c1deee435d665976aba4b47dd2f1cd3cfe180f8d87e15b907',
     port: 5432,
     pool: {
       max: 5,
