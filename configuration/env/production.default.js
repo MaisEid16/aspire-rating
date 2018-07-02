0dc44878c963228255806af1cf43d4aacad20236
Mais Eid <meid@aspire-services.net>
2018-07-02 13:25:49 +0300

    update

--- configuration/env/production.default.js
+++ configuration/env/production.default.js
@@ -1,6 +1,6 @@
 module.exports = {
 	verificationToken: 'El8v1xtuFnYGE1RuuAYPljtF',
-	oAuthAccessToken: 'xoxp-363603873302-363759356951-379163587523-b59256f19ecc1d0f6db14de74850c502',
+	oAuthAccessToken: 'xoxp-363603873302-362849389029-391303855603-1994867bba39cad547fd99c43abc3c6c',
 	botToken: 'xoxb-363603873302-365443179825-vygYjm3IYTu2At96j0XxRJja',
 	tragetedChannel: '#test',
 	maxNumberOfStarsPerMonth: 5,
