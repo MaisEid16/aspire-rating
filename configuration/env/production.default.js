d3e6f39b824f54545e8a0e8e5a17dc817c1b9f31
Mais Eid <meid@aspire-services.net>
2018-07-23 14:47:14 +0300

    commit 33

--- configuration/env/production.default.js
+++ configuration/env/production.default.js
@@ -1,6 +1,6 @@
 module.exports = {
 	verificationToken: 'El8v1xtuFnYGE1RuuAYPljtF',
-	oAuthAccessToken: 'xoxp-363603873302-362849389029-404169646215-33a53943dcdfeb70d7f478156732036f',
+	oAuthAccessToken: 'xoxp-363603873302-362849389029-402466610672-e66bda86a1c984dba0d761fb19710e8d',
 	botToken: 'xoxb-363603873302-365443179825-vygYjm3IYTu2At96j0XxRJja',
 	tragetedChannel: '#rating',
 	maxNumberOfStarsPerMonth: 5,
