973b26d6e5569aff84b1eb4956e8f8fcfa718a6a
Mais Eid <meid@aspire-services.net>
2018-07-23 13:45:15 +0300

    commit 22

--- configuration/env/production.default.js
+++ configuration/env/production.default.js
@@ -1,8 +1,8 @@
 module.exports = {
 	verificationToken: 'El8v1xtuFnYGE1RuuAYPljtF',
-	oAuthAccessToken: 'xoxp-363603873302-362849389029-391303855603-1994867bba39cad547fd99c43abc3c6c',
+	oAuthAccessToken: 'xoxp-363603873302-362849389029-404169646215-33a53943dcdfeb70d7f478156732036f',
 	botToken: 'xoxb-363603873302-365443179825-vygYjm3IYTu2At96j0XxRJja',
-	tragetedChannel: '#test',
+	tragetedChannel: '#rating',
 	maxNumberOfStarsPerMonth: 5,
 	starIcon: ':star2:',
 	nomineesCount: 3
